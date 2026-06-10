d7f232061d2d60347746024e670aa5f7e1fd1cf1
nik <paln48099@gmail.com>
2026-06-10 21:11:04 +0000

    PHASE2 : USER CREATION AND OWNERSHIP

--- core_ops.sh
+++ core_ops.sh
@@ -1,25 +1,81 @@
 #!/bin/bash
 
-         #1 : First we check for no. of  arguments
-	  
-         if [ $# -ne 1 ]
-         then 
-              
-	    echo " usage: ./core_ops.sh < init_env > "
+                                                                
 
-	    exit 1
-
-
-	 fi 
-
-        #2 : We check for the valid argument 
+                                                                            
           
-        first_arg=$1 
- 
-        if [ "$first_arg" == init_env ]         
-        then		
-       	    echo " HII USER Welcome to this Environment " 
+        Arguments=$1 
 
+         USER_ENTRY() {                
+		      
+		       USERNAME=$1
+		       SECTION=$2
+      
+		      if [  $# -ne 2 ] 
+		      then 
+			      echo " ERROR!! PLEASE FILL THE DETAILS AGAIN "
+
+			      echo " USAGE : <USERNAME> <SECTION> "
+
+		              exit 1
+		     else
+		          echo " PLEASE WAIT FOR FEW SECONDS SYSTEM IS CHECKING FOR THE GIVEN USERNAME: '$USERNAME' AND SECTION : '$SECTION' " 
+		      fi 
+
+
+		      if id "$USERNAME" >/dev/null 2>&1
+
+		      then 
+			   echo " USER: "$1" ALREADY EXIST ON THIS SERVER "
+			   exit 1
+		   else    
+			    
+			    sudo useradd -m  "$USERNAME"       			      
+			      
+			    sudo usermod -g devops_team "$USERNAME"
+
+			    sudo delgroup $USERNAME
+
+			    sudo usermod -aG PROJECT_CHARLIE "$USERNAME"
+
+			      
+                              if [ ! -d "/home/$USERNAME/$SECTION" ]
+			      then 
+				   sudo mkdir -p "/home/$1/$2"
+			      fi	    
+			      	      
+
+			       
+			     sudo chown "$USERNAME":devops_team "/home/$USERNAME/$SECTION"
+
+
+			     echo  " USER : '$1' IS SUCCESSFULLY LOGGED IN THE SERVER IN THE SECTION : '$SECTION' "  
+
+
+		      fi 
+
+
+		      
+		      
+		      
+		      }
+
+
+
+
+
+
+        if [ "$Arguments" == init_env ]         
+        then		
+       	         if [ $# -ne 1 ] 
+		 then
+                    echo "Usage: ./core_ops.sh <init_env>"
+
+                    exit 1
+
+                 fi
+          
+             echo " HII USER WELCOME TO THIS ENVIRONMENT "
 
 
 	     
@@ -34,7 +90,7 @@
 
            
 
-                   #3 : Now we check for the existing directory
+                                                                         #3 : Now we check for the existing directory
 
                     if [ ! -d "/opt/devops/logs" ]
                     then
@@ -43,7 +99,7 @@
 	
                      fi
 
-		   #4 : Check for the Network Status
+		                                                              #4 : Check for the Network Status
 
 
 	            LOCAL_IP=$(hostname -I | awk '{print$1}') 
@@ -56,16 +112,79 @@
 
                       else 
                       echo " ALERT !!!!! : YOUR SYSTEM NETWORK INTERFACE IS NOT GOOD. ENSURE A GOOD NETWORK CONNECTION "
-                      fi 		
+
+                      fi 
+
+
+
+             elif [ "$Arguments" == init_login ]
+	     then
+
+		     if [ $# -ne 3 ]
+		      then 
+		           echo " ERROR!! USAGE: ./core_ops.sh <init_login> <USERNAME> <SECTION> "
+	              exit 1
+                      fi 
+                    
+
+
+                USER_ENTRY "$2" "$3"
 
 
 
 
- else 
-	      echo " Please enter valid ARGUMENT to access "
-	      exit 1 
-         fi
+	else 
+            echo " PLEASE ENTER VALID ARGUMENT TO ACCESS "		
+	    echo "          HELP GUIDE 
+	               
+	            TO CHECK ENV  >>>>    USAGE : ./core_ops.sh <init_env>
+
+		    TO LOGIN      >>>>    USAGE : ./core_ops.sh <init_login> <username> <section name> "
+
+	fi
 
 
 
-         	     
+     
+             		      
+
+
+
+
+          
+	         
+	         
+             
+
+			    		      
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+                      	     
